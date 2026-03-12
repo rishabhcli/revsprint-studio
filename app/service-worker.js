7f5d86dcdac81f235df2b509e7b20d1d21bdbf07
Rishabh Bansal <rishabh.rb@icloud.com>
2026-03-12 03:11:19 -0700

    feat: add offline shell caching

---
+++ app/service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "revsprint-studio-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./icon.svg",
+  "./manifest.json",
+  "./service-worker.js",
+  "./src/main.js",
+  "./src/engine.js",
+  "./src/state.js",
+  "./src/storage.js",
+  "./src/brief.js",
+  "./src/demo.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((response) => {
+          if (!response.ok) {
+            return response;
+          }
+
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
